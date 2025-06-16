100c502813651da549c3770eee70c883e5bcb2a3
GANESHKUMMITHI <ganeshkummithi5129@gmail.com>
2025-06-16 21:29:20 +0530

    Update Starrightangle.java

--- Starrightangle.java
+++ Starrightangle.java
@@ -1,14 +1,1 @@
-public class Main4{
-    public static void main(String args[]){
-        int k=10;
-        starangle(k);
-    }
-    public static void starangle(int k){
-        for(int i=0;i<k;i++){
-            for(int j=i;j<k;j++){
-                System.out.print(" * ");
-            }
-            System.out.println();
-        }
-    }
-}
+
